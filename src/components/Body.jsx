358ac54640d295a6e9492b2d0fe3c6c2b76cb1b0
Lalit Kumar <lalitkumar@Lalits-MacBook-Air.local>
2025-03-24 12:51:14 +0530

    added css and routing

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,25 @@
+import { createBrowserRouter } from "react-router-dom";
+import { Browse } from "./Browse";
 import { Login } from "./Login"
+
+//1. Create browserRounter for Login and Browse components.
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Login />
+    },
+    {
+        path: "/browse",
+        element: <Browse />
+    }
+]);
+//2. Use RouterProvider to provide the router to the components.
 
 export const Body = () => {
     return (
     <>
-    <Login></Login>
+    <Login />
+    <Browse />
     </>
     );
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,27 +1,32 @@
 import { useState } from "react";
 import { Header } from "./Header";
+import '../App.css'
 
- export const Login = () => {
-    const [isSignIn, setIsSignIn] = useState(true);
+export const Login = () => {
+  const [isSignIn, setIsSignIn] = useState(true);
 
-    const clickHandler = () => {
-        setIsSignIn(!isSignIn);
-    }
+  const clickHandler = () => {
+    setIsSignIn(!isSignIn);
+  };
 
-    return (
-        <>
-        <div><Header/></div>
-        
-        <form >
+  return (
+    <>
+      <div>
+        <Header /> 
+      </div>
+
+      <form>
         <h1>{isSignIn ? "Sign In" : "Sign up"}</h1>
-        {isSignIn && <input type="textbox" placeholder="Email"/>}
-        <input type="textbox" placeholder="Email"/>
-        <input type="textbox" placeholder="Password"/>
+        {isSignIn && <input type="textbox" placeholder="Full Name" />}
+        <input type="textbox" placeholder="Email" />
+        <input type="textbox" placeholder="Password" />
         <button>Sign</button>
-        <p onClick={clickHandler}>{isSignIn ? "New to Netflix? Sign Up Now" : "Already rigistered, Sign In Now"}</p>
-        </form>
-        
-        </>
-        
-    );
-}
+        <p onClick={clickHandler}>
+          {isSignIn
+            ? "New to Netflix? Sign Up Now"
+            : "Already rigistered, Sign In Now"}
+        </p>
+      </form>
+    </>
+  );
+};
